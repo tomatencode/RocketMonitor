c652a75789bcc2868e3ce15c518f732d929cb089
tomatencode <ingo.thomas@gmx.com>
2026-09-11 12:57:54 +0200

    updated rocket link protocoll

--- src/shared/styles.ts
+++ src/shared/styles.ts
@@ -9,6 +9,5 @@ export const logBackground = "bg-zinc-900/60";
 export const filterBackground = "bg-zinc-700/60";
 
 export const dividerBorder = "border-zinc-700/50";
-export const inputBorder = "border-zinc-600";
 export const filterBorder = "border-zinc-600";
 export const mutedBorder = "border-zinc-700";
